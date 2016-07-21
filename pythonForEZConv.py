b549a81b4881c80c965b60fab0e6667bdea3b75a
lgardner462 <lgardner@techsquare.com>
2016-07-21 17:12:45 -0400

    Update pythonForEZConv.py

--- pythonForEZConv.py
+++ pythonForEZConv.py
@@ -8,17 +8,18 @@ from collections import defaultdict
 
 #Open CSV file
 try:
-	rackCSVreader = csv.reader(open(sys.argv[1],'rU'),skipinitialspace = True)
+	rackCSVreader = csv.reader(open(sys.argv[1],'rU'),quotechar="'",delimiter=',',skipinitialspace = True)
 except(IOError):
 	print("Inventory CSV file must be supplied in order to proceed.") 
 	sys.exit(0)
-try:
-	dtCSVreader = csv.reader(open("DeviceTypes",'rU'),skipinitialspace=True)
-except(IOError):
-	print("Device Types csv not found, this script uses the csv file 'DeviceTypes' to create RackTables objects.") 
-	sys.exit(0)
+#try:
+#	dtCSVreader = csv.reader(open("DeviceTypes",'rU'),skipinitialspace=True)
+#except(IOError):
+#	print("Device Types csv not found, this script uses the csv file 'DeviceTypes' to create RackTables objects.") 
+#	sys.exit(0)
 
-deviceDictFull = {}
+#deviceDictFull = {}
+orig_stdout = sys.stdout
 rowCounter = 0
 deviceName = []
 rowNo = []
@@ -30,12 +31,13 @@ deviceType = []
 primaryPool = []
 primaryGroup = []
 fullCSV = []
-for row in dtCSVreader:
-	try:
-		deviceDictFull.setdefault(row[0],[]).append(row[1])
+objectType=[]
+#for row in dtCSVreader:
+#	try:
+#		deviceDictFull.setdefault(row[0],[]).append(row[1])
 			
-	except(IndexError):
-		pass
+#	except(IndexError):
+#		pass
 for row in rackCSVreader:
 	try:
 		fullCSV.append(row)
@@ -46,25 +48,38 @@ for row in rackCSVreader:
 		uLo.append((row[4:5][0].replace("'","")))
 		uHi.append((row[5:6][0].replace("'","")))
 		deviceType.append(row[6:7][0].replace("'",""))
-		primaryPool.append(row[7:8][0].replace("'","").replace('"',"").replace("ppool:","").strip("{} "))
-		fPGroup=((str(row[8:][0:])).replace("{pgroup:","").rstrip('"}\\\'[]')).lstrip("['").replace('"','').replace("'",'')
+		fPpool=((str(row[7:8][0]).split(",",1)[0])).replace('{ppool: "',"").rstrip('"}')
+		primaryPool.append(fPpool)
+		#primaryPool.append(row[7:8][0].replace("'","").replace('"',"").replace("ppool:","").strip("{} "))
+		#fPGroup=((str(row[8:][0:])).replace("{pgroup:","").rstrip('"}\\\'[]')).lstrip("['").replace('"','').replace("'",'')
+		fPGroup=((str(row[7:8][0]).split(",",1)[1])).replace('{pgroup: "',"").rstrip('"} ')		
 		primaryGroup.append(fPGroup)
+		objectType.append(row[8:9][0])
 		rowCounter+=1
 	except(IndexError):
 		pass
-#print(deviceDictFull)
-print("RACK;MIT;MGHPCC;"+str("r"+rowNo[0].strip()+"-p"+podNo[0].strip()).upper() + ";" +"C"+cabNo[0].strip())
+
+#print(primaryGroup)
+
+locSTR=(str("r"+rowNo[0].strip()+"-p"+podNo[0].strip()).upper() + "-" +"C"+cabNo[0].strip())
+outFile = (locSTR +"-csv.txt")
+f = file(outFile, 'w')
+sys.stdout = f
+
+print("RACK;MIT;MGHPCC;"+locSTR)
+
 for i in range(rowCounter):
 	try:	
 		uRange= str(range((int(uLo[i])),(int(uHi[i]))+1)).strip("[]").replace(" ","")
 		fLoc=str("r"+rowNo[i].strip()+"-p"+podNo[i].strip()+"-c"+cabNo[i]).upper()
-		#print(deviceName[i]+ "loc="+fLoc+ " Unumbers="+uRange+" DEVICETYPE="+deviceType[i]+" PrimaryPool="+primaryPool[i]+" primaryGroup="+primaryGroup[i])
+		#print("DEBUG" + deviceName[i]+ "loc="+fLoc+ " Unumbers="+uRange+" DEVICETYPE="+deviceType[i]+" PrimaryPool="+primaryPool[i]+" primaryGroup="+primaryGroup[i])
 		#template="{deviceName:40}|{Pool:20}|{deviceType:30}"
 		#print template.format(deviceName=deviceName[i],deviceType=deviceType[i],Pool=primaryPool[i])
-		print("OBJECT;"+ str(deviceDictFull[deviceType[i].strip()]).lstrip("['").rstrip("']").upper() +";"+ deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +(deviceType[i].strip())+ ";"+deviceName[i].strip()+"-"+primaryPool[i].strip())
+		print("OBJECT;"+ objectType[i]+";"+ deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +(deviceType[i].strip())+ ";"+deviceName[i].strip()+"-"+primaryPool[i].strip())
 		print("OBJECTTAG;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +primaryPool[i].strip()))
-		print("OBJECTATTRIBUTE;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" + "COMMENT;"+ "Primary Pool: " + primaryPool[i].strip() + "    Primary Group: " + primaryGroup[i]))
+		print("OBJECTATTRIBUTE;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" + "COMMENT;"+ "Primary Pool: " + primaryPool[i] + "    Primary Group: " + primaryGroup[i]))
 		print("RACKASSIGNMENT;"+deviceName[i].strip()+"-"+primaryPool[i].strip()+";"+"C"+cabNo[0].strip()+";"+uRange)+";"+((len(range(int(uLo[i]),int(uHi[i]))))*"fib,"+"fib")
 	except(ValueError):
 		pass		
-	
+sys.stdout = orig_stdout
+f.close()	
