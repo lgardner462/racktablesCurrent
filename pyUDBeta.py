16baf5b9d3196aeed02046f3274dbd047fc6a084
lawrence <lgardner462@hcc.edu>
2016-08-04 17:56:33 -0400

    Working on UD script, added backup SQL

--- pyUDBeta.py
+++ pyUDBeta.py
@@ -12,6 +12,11 @@ try:
 except(IOError):
 	print("Inventory CSV file must be supplied in order to proceed.") 
 	sys.exit(0)
+try:
+	rackCSVreader2 = csv.reader(open(sys.argv[2],'rU'),quotechar="'",delimiter=',',skipinitialspace = True)
+except(IOError):
+	print("Inventory CSV file must be supplied in order to proceed.") 
+	sys.exit(0)
 #try:
 #	dtCSVreader = csv.reader(open("DeviceTypes",'rU'),skipinitialspace=True)
 #except(IOError):
@@ -32,6 +37,7 @@ primaryPool = []
 primaryGroup = []
 fullCSV = []
 objectType=[]
+ print(list(set(rackCSVreader) - set(rackCSVreader)))
 #for row in dtCSVreader:
 #	try:
 #		deviceDictFull.setdefault(row[0],[]).append(row[1])
@@ -61,25 +67,45 @@ for row in rackCSVreader:
 
 #print(primaryGroup)
 
-locSTR=(str("r"+rowNo[0].strip()+"-p"+podNo[0].strip()).upper() + "-" +"C"+cabNo[0].strip()))
-outFile = (locSTR +"-csv.txt")
+locSTR=(str("r"+rowNo[0].strip()+"-p"+podNo[0].strip()).upper() + "-" +"C"+cabNo[0].strip())
+outFile = ("UPDATED" +"-initialize-objects-csv.txt")
 f = file(outFile, 'w')
 sys.stdout = f
 
-print("RACK;MIT;MGHPCC;"+locSTR)
+
+#cabinetName= str("r"+rowNo[0].strip()+"p"+podNo[0].strip()).lower()+"c"+cabNo[0].strip()
+
+rackName=str("r"+rowNo[0].strip()+"-p"+podNo[0].strip()).upper()
+cabinetName = "C"+cabNo[0].strip()
+#print("RACK;MIT;MGHPCC;"+rackName+";"+cabinetName)
+for i in range(rowCounter):
+	try:	
+		
+		#print("OBJECT;"+ str(objectType[i]).upper()+";"+ deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +(deviceType[i].strip())+ ";"+deviceName[i].strip()+"-"+primaryPool[i].strip())
+		#print("OBJECTTAG;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +primaryPool[i].strip()))
+		#print(primaryPool[i].strip())
+	except(ValueError):
+		pass
+	
+outFile = ("UPDATED"+"-update-csv.txt")
+g = file(outFile, 'w')
+sys.stdout = g
 
 for i in range(rowCounter):
 	try:	
+		rackName=str("r"+rowNo[i].strip()+"-p"+podNo[i].strip()).upper()
+		cabinetName = "C"+cabNo[i].strip()
+		#print("RACK;MIT;MGHPCC;"+rackName+";"+cabinetName)
 		uRange= str(range((int(uLo[i])),(int(uHi[i]))+1)).strip("[]").replace(" ","")
 		fLoc=str("r"+rowNo[i].strip()+"-p"+podNo[i].strip()+"-c"+cabNo[i]).upper()
 		#print("DEBUG" + deviceName[i]+ "loc="+fLoc+ " Unumbers="+uRange+" DEVICETYPE="+deviceType[i]+" PrimaryPool="+primaryPool[i]+" primaryGroup="+primaryGroup[i])
 		#template="{deviceName:40}|{Pool:20}|{deviceType:30}"
 		#print template.format(deviceName=deviceName[i],deviceType=deviceType[i],Pool=primaryPool[i])
-		print("OBJECT;"+ objectType[i]+";"+ deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +(deviceType[i].strip())+ ";"+deviceName[i].strip()+"-"+primaryPool[i].strip())
-		print("OBJECTTAG;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" +primaryPool[i].strip()))
-		print("OBJECTATTRIBUTE;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" + "COMMENT;"+ "Primary Pool: " + primaryPool[i] + "    Primary Group: " + primaryGroup[i]))
-		print("RACKASSIGNMENT;"+deviceName[i].strip()+"-"+primaryPool[i].strip()+";"+"C"+cabNo[0].strip()+";"+uRange)+";"+((len(range(int(uLo[i]),int(uHi[i]))))*"fib,"+"fib")
+		
+		#print("OBJECTATTRIBUTE;"+str(deviceName[i].strip()+"-"+primaryPool[i].strip()+ ";" + "COMMENT;"+ "Primary Pool: " + primaryPool[i] + "    Primary Group: " + primaryGroup[i]))
+		#print("RACKASSIGNMENT;"+deviceName[i].strip()+"-"+primaryPool[i].strip()+";"+cabinetName+";"+uRange)+";"+((len(range(int(uLo[i]),int(uHi[i]))))*"fib,"+"fib;" + rackName)
 	except(ValueError):
-		pass		
+		pass	
+
 sys.stdout = orig_stdout
 f.close()	
